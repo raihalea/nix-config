0f75dd4fe70757705a47057a59319562763348ed
raiha <68338762+raihalea@users.noreply.github.com>
2026-05-24 23:55:05 +0900

    update zshrc

--- modules/common/shell.nix
+++ modules/common/shell.nix
@@ -38,6 +38,18 @@ in
       elif [ -f "/usr/local/bin/brew" ]; then
         eval "$(/usr/local/bin/brew shellenv)"
       fi
+
+      # ghq + fzf
+      function ghq-fzf() {
+        local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+        if [ -n "$src" ]; then
+          BUFFER="cd $(ghq root)/$src"
+          zle accept-line
+        fi
+        zle -R -c
+      }
+      zle -N ghq-fzf
+      bindkey '^g' ghq-fzf
     '';
   };
 
